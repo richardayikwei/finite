656468c6c1c7c11720e299b5e379c5698e33759c
richardayikwei <richardayikweibee@gmail.com>
2026-02-14 09:42:07 +0000

    refactor(password_generator.py): add an exception to handle input less than 10

--- password_generator.py
+++ password_generator.py
@@ -1,8 +1,11 @@
 import random
 import string
 
+class InputTooSmallError(Exception):
+    '''Raised when the input value is less than 10'''
+    pass
 
-def password_generator():
+def password_generator(pass_len=10):
     # Sources of data
     letters = string.ascii_letters
     numbers = string.digits
@@ -10,8 +13,10 @@ def password_generator():
 
     # Combile into one string
     letters_numbers = letters + numbers + symbols
-
-    password = random.choices(letters_numbers, k=10)
+    
+   # if pass_len < 10:
+    #    raise 
+    password = random.choices(letters_numbers, k=75)
 
     return ''.join(password)
 
