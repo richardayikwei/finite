7241f38db86e53c59ed4eb182b5fe34278bb7015
richardayikwei <richardayikweibee@gmail.com>
2026-02-16 09:11:29 +0000

    test(test_password_generator.py): add test for TypeError exceptions
    The program should raise an error if the input provided is a float

--- finite_core/tests/test_password_generator.py
+++ finite_core/tests/test_password_generator.py
@@ -5,3 +5,7 @@ class TestErrors:
     def test_input_too_small_error(self):
         with pytest.raises(InputTooSmallError):
             password_engine(9)
+
+    def test_input_is_a_float_error(self):
+        with pytest.raises(TypeError):
+            password_engine(10.5)
