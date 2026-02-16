149b4ad786a63465b730b211d53bc6ac34867857
richardayikwei <richardayikweibee@gmail.com>
2026-02-16 08:58:20 +0000

    feat(password_generator.py): add handling for float input
    The input should be an integer always. Feature handles if it's a float

--- finite_core/src/password_generator.py
+++ finite_core/src/password_generator.py
@@ -17,6 +17,9 @@ def password_engine(pass_len=10):
     if pass_len < 10:
         raise InputTooSmallError('Password length should be greater than 9')
 
+    if not pass_len.is_integer():
+        raise TypeError('Password should not be a decimal')
+
     password = random.choices(letters_numbers, k=pass_len)
 
     return ''.join(password)
