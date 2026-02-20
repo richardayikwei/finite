e4bfa5c748a949026f553977a86903161bd96b28
richardayikwei <richardayikweibee@gmail.com>
2026-02-20 15:02:58 +0000

    feat(main.py): add fastapi routing
    app can not take input and send out put in json

--- app/password_generator.py
+++ app/password_generator.py
@@ -5,7 +5,7 @@ class InputTooSmallError(Exception):
     '''Raised when the input value is less than 10'''
     pass
 
-def password_engine(pass_len):
+def password_engine(pass_len:int):
     # Sources of data
     letters = string.ascii_letters
     numbers = string.digits
@@ -17,8 +17,8 @@ def password_engine(pass_len):
     if pass_len < 10:
         raise InputTooSmallError('Password length should be greater than 9')
 
-    if not pass_len.is_integer():
-        raise TypeError('Password should not be a decimal')
+    if not isinstance(pass_len, int):
+       raise TypeError('Password should not be a decimal')
 
     password = random.choices(letters_numbers, k=pass_len)
 
