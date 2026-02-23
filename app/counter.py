93025bee41b712ebed36dbc133ee3befea1caef8
richardayikwei <richardayikweibee@gmail.com>
2026-02-23 19:23:34 +0000

    feat(test_counter.py): add test for connection to database
    the test connect to neon test_password counter database to run test instead of production data base

--- app/counter.py
+++ app/counter.py
@@ -8,18 +8,18 @@ def get_connection(database = "DATABASE_URL"):
     conn = psycopg2.connect(os.getenv(database))
     return conn
 
-def increment_count_manager(func, connect_info = get_connection()):
+def increment_count_manager(func = None, connect_info = get_connection() , database_name="password_counter"):
     def proxy(*args,**kwargs):
-        query = f'UPDATE password_counter SET clicks = clicks + 1 WHERE id = %s;'
+        query = f'UPDATE {database_name} SET clicks = clicks + 1 WHERE id = %s;'
         with connect_info.cursor() as cur:
             cur.execute(query, (1,))
-        connect_info.commit()  
+            connect_info.commit()  
 
         return func(*args, **kwargs)
     return proxy
 
-def get_count_manager(connect_info = get_connection()):
-    query = f'SELECT clicks FROM password_counter WHERE id = %s;'
+def get_count_manager(connect_info = get_connection(), database_name="password_counter"):
+    query = f'SELECT clicks FROM {database_name} WHERE id = %s;'
     with connect_info.cursor() as cur:
         cur.execute(query, (1,))
         return cur.fetchone()[0]
