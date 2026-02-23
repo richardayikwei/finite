f2b089fc3c92d3d2c50a601a35c753f52cc1b11f
richardayikwei <richardayikweibee@gmail.com>
2026-02-23 18:50:17 +0000

    refactor(counter.py): add context manager to ensure connection closes

--- app/counter.py
+++ app/counter.py
@@ -6,21 +6,20 @@ load_dotenv()
 
 def get_connection(database = "DATABASE_URL"):
     conn = psycopg2.connect(os.getenv(database))
-    cur = conn.cursor()
-    return cur, conn
+    return conn
 
 def increment_count_manager(func, connect_info = get_connection()):
     def proxy(*args,**kwargs):
         query = f'UPDATE password_counter SET clicks = clicks + 1 WHERE id = %s;'
-        cur, conn = connect_info
-        cur.execute(query, (1,))
-        conn.commit()  
+        with connect_info.cursor() as cur:
+            cur.execute(query, (1,))
+        connect_info.commit()  
 
         return func(*args, **kwargs)
     return proxy
 
 def get_count_manager(connect_info = get_connection()):
     query = f'SELECT clicks FROM password_counter WHERE id = %s;'
-    cur, conn = connect_info
-    cur.execute(query, (1,))
-    return cur.fetchone()[0]
+    with connect_info.cursor() as cur:
+        cur.execute(query, (1,))
+        return cur.fetchone()[0]
--- tests/test_counter.py
+++ tests/test_counter.py
@@ -1,4 +1,11 @@
 from app.counter import increment_count_manager, get_count_manager
+import pytest
+import os
+import psycopg2
+
+@pytest.fixture(scope="module")
+
+
 
 class DabaseChanges:
     def test_increment_count_manager():
