4053f14b15c3b33c5f4e18a0e6eb67d6c4ab741c
richardayikwei <richardayikweibee@gmail.com>
2026-02-23 18:10:17 +0000

    refactor(counter.py): add load .env variable and pass connection as function to code

--- app/counter.py
+++ app/counter.py
@@ -1,28 +1,26 @@
 import psycopg2
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-conn = psycopg2.connect(os.getenv("DATABASE_URL"))
-cur = conn.cursor()
+def get_connection(database = "DATABASE_URL"):
+    conn = psycopg2.connect(os.getenv(database))
+    cur = conn.cursor()
+    return cur, conn
 
-def increment_count_manager(func):
+def increment_count_manager(func, connect_info = get_connection()):
     def proxy(*args,**kwargs):
         query = f'UPDATE password_counter SET clicks = clicks + 1 WHERE id = %s;'
+        cur, conn = connect_info
         cur.execute(query, (1,))
         conn.commit()  
 
         return func(*args, **kwargs)
     return proxy
 
-def get_count_manager():
+def get_count_manager(connect_info = get_connection()):
     query = f'SELECT clicks FROM password_counter WHERE id = %s;'
+    cur, conn = connect_info
     cur.execute(query, (1,))
     return cur.fetchone()[0]
-
-@increment_count_manager
-def hack(a,b,c):
-    # print(a + b + c)
-    pass
-
-# hack(1, 2, 3)
-hack(4, 5, 6)
---
+++ tests/test_counter.py
@@ -0,0 +1,5 @@
+from app.counter import increment_count_manager, get_count_manager
+
+class DabaseChanges:
+    def test_increment_count_manager():
+        pass
